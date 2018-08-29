4695c9a6f1311e244bab1efe88458157bd92d9d6
Juriy Panasevich <elfelrandcor@gmail.com>
2018-08-29 15:52:17 +0300

    Повышение частоты обновления статусов модели

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('model:status')->everyFiveMinutes();
+         $schedule->command('model:status')->everyMinute();
     }
 
     /**
